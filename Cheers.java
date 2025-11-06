df6a3ab794404ab8d8bf7d49e82cf09db77a7754
Nitsan Tsalka <nitsan.tsalka@post.runi.ac.il>
2025-11-06 21:26:27 +0200

    finished exc

--- Cheers.java
+++ Cheers.java
@@ -22,7 +22,7 @@ public class Cheers {
                      System.out.println("Give me an " + letter +": " + letter + "!");   
                 }
                 else{
-                     System.out.println("Give me a " + letter +": " + letter + "!");
+                     System.out.println("Give me a  " + letter +": " + letter + "!");
                 }
                 i++;
           }  
